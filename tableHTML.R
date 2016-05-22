b8edb8cfe27893f221b9b7d9e4d6b1f4736be5ec
Teo <teoboot2007@hotmail.com>
2016-05-22 23:24:19 +0100

    function core

--- tableHTML.R
+++ tableHTML.R
@@ -1,30 +1,51 @@
-HTMLtable <- function(obj, rownames=TRUE, class=paste0('table_',deparse(substitute(obj))), second_header = NULL){
+HTMLtable <- function(obj, 
+                      rownames = TRUE,
+                      class = paste0('table_', deparse(substitute(obj))),
+                      second_header = NULL) {
      
-     if(rownames==TRUE){
-       headers <- paste0('<tr><th id=header_1> </th>', 
-                         paste0(vapply(seq_along(names(obj)) + 1, function(x){paste0('<th id=header_', x, '>', names(obj)[x - 1], '</th>')}
-                                       , FUN.VALUE=character(1)),
-                         collapse=''),
-                         '</tr>\n')
-     }else{
-       headers <- paste0('<tr>', 
-                         paste0(vapply(seq_along(names(obj)), function(x){paste0('<th id=header_', x, '>', names(obj)[x], '</th>')}
-                                       , FUN.VALUE=character(1)),
+     if (rownames == TRUE) {
+       headers <- paste0('<tr> <th id = header_1> </th>', 
+                         paste0(vapply(seq_along(names(obj)) + 1,
+                                       function(x){
+                                        paste0('<th id=header_', x, '>',
+                                               names(obj)[x - 1],
+                                               '</th>')
+                                        },
+                                       FUN.VALUE=character(1)),
                                 collapse=''),
+                         '</tr>\n')
+     } else {
+       headers <- paste0('<tr>', 
+                         paste0(vapply(seq_along(names(obj)), 
+                                       function(x) {
+                                        paste0('<th id = header_', 
+                                               x, 
+                                               '>',
+                                               names(obj)[x],
+                                               '</th>') 
+                                        },
+                                       FUN.VALUE = character(1)),
+                                collapse = ''),
                          '</tr>\n')
      }
      
-     if(!is.null(second_header)){
+     if (!is.null(second_header)) {
        over_header <- 
          paste0('<tr>', 
-                paste0(vapply(seq_along(second_header), function(x){
-                  paste0('<th colspan=', second_header[x] , ' id=overheader_', x, '>',
-                         names(second_header)[x],
-                         '</th>')
-                }, FUN.VALUE=character(1)),
-                collapse=''),
+                paste0(vapply(seq_along(second_header), 
+                              function(x){
+                               paste0('<th colspan =', 
+                                      second_header[x],
+                                      ' id = overheader_', 
+                                      x,
+                                      '>',
+                               names(second_header)[x],
+                               '</th>')
+                              }, 
+                              FUN.VALUE = character(1)),
+                       collapse = ''),
                 '</tr>\n')
-     }else{
+     } else {
        over_header <- NULL
      }
      
@@ -32,12 +53,22 @@ HTMLtable <- function(obj, rownames=TRUE, class=paste0('table_',deparse(substitu
        paste0('<td align="right" id="', x, '">', obj[[x]], '</td>')
      })
      
-     if(rownames == TRUE) {
-       content <- c(list(paste0('<td align="right" id="rownames">', row.names(obj), '</td>')), content)
+     if (rownames == TRUE) {
+       content <- c(list(paste0('<td align="right" id="rownames">', 
+                                row.names(obj), 
+                                '</td>')), 
+                    content)
      }
      
      content <- cbind('<tr>', do.call(cbind, content), '</tr>\n')
      content <- paste(apply(content, 1, paste, collapse=''), collapse='')
      
-     HTML(paste0('<table class=', class, ' border=1>\n', over_header, headers, content, '</table>', collapse=''))
+     HTML(paste0('<table class=', 
+                 class, 
+                 ' border=1>\n', 
+                 over_header, 
+                 headers, 
+                 content, 
+                 '</table>', 
+                 collapse=''))
    }
