c90ca1daec9a5ca56b622ba7c80ba8b6e3b3d87a
Teo <teoboot2007@hotmail.com>
2016-07-01 22:33:21 +0100

    added make_css function

---
+++ R/make_css.R
@@ -0,0 +1,34 @@
+#' Create a css file
+#'
+#' \code{make_css} will create a css file which can also be used in shiny
+#' 
+#' \code{make_css} will create a css file which can also be used in shiny
+#'
+#' @param ... css style definitions
+#'
+#' @return A css file. 
+#'         
+#' @examples
+#' make_css(list('table', c('text-align', 'font-size'), c('center', '20px')),
+#'          list('th', c('text-align', 'font-size'), c('center', '20px')))
+#' 
+#' @export
+make_css <- function(...) {
+ 
+ css_defs <- list(...)
+ 
+ all_css <- 
+   lapply(css_defs, function(x) {
+    
+     css_comp <- paste0(x[[2]], ':', x[[3]], ';')
+     style <- paste(css_comp, collapse = '\n  ')
+     
+     paste0(x[[1]], 
+            ' {\n  ',
+            style,
+            '\n}\n')
+    
+   })
+ 
+ HTML(paste(all_css, collapse = '\n'))
+}
