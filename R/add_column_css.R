950f906aef299276fa248300ca20945795129006
Teo <teoboot2007@hotmail.com>
2017-05-28 15:19:50 +0100

    added more content to description and changed version

--- R/add_column_css.R
+++ R/add_column_css.R
@@ -46,7 +46,7 @@
 #'           row_groups = list(c(10, 10, 12), c('Group 1', 'Group 2', 'Group 3')),
 #'           second_headers = list(c(3, 4), c('col1', 'col2'))) %>% 
 #'   add_css_column(list('background-color', 'green'), -1) %>%
-#'   add_css_column(list('background-color', 'red'), 0)
+#'   add_css_column(list('background-color', 'red'), c(0, 1))
 #'
 #' @export
 add_css_column <- function(tableHTML, 
