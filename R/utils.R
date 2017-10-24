4c91520246fde7adfb967d88c12225c1ea92d0b1
clemens-zauchner <cst.zauchner@gmail.com>
2017-10-24 07:58:01 +0100

    replace_HTML duplicate bug dix

--- R/utils.R
+++ R/utils.R
@@ -1,7 +1,7 @@
 is.fachar <- function(x) is.character(x) | is.factor(x)
 
 
-replace_style <- function(tableHTML, split, style, condition = NULL){
+replace_style<- function(tableHTML, split, style, condition = NULL){
   
   if (is.null(condition)) {
     condition <- rep(TRUE, attr(tableHTML, "nrows"))
@@ -11,16 +11,17 @@ replace_style <- function(tableHTML, split, style, condition = NULL){
                     paste0(split), tableHTML)
   
   splits <- strsplit(tableHTML, split)
-
+  
   splits[[1]][2:length(splits[[1]])] <- 
-    vapply(splits[[1]][2:length(splits[[1]])], function(x) {
+    vapply(2:length(splits[[1]]), function(i) {
+      x <- splits[[1]][i]
       #check if style should be applied
-      
-      if (condition[which(splits[[1]] %in% x) - 1]) {
-        paste0(split,' ', style[which(splits[[1]] %in% x) - 1], x)
+      if (condition[i - 1]) {
+        paste0(split,' ', style[i - 1], x)
       } else {
         #if not, check if style was applied from previous functions
-        if (substr(x, 1, 1) == ">") {
+        # or if rowspan attribute present
+        if (substr(x, 1, 1) == ">" | substr(x, 1, 8) == " rowspan") {
           paste0(split, x)
         } else {
           paste0(split, ' style=', x)
@@ -28,13 +29,12 @@ replace_style <- function(tableHTML, split, style, condition = NULL){
       }
       
     }, FUN.VALUE = character(1))
- 
+  
   gsub(';""', ';', paste(splits[[1]], collapse = ''))
- 
+  
 }
 
 convert_type <- function(v, type, levels) {
-  
   switch(type,
          numeric = as.numeric(v),
          integer = as.integer(v),
