5651ead8c3201708edb03f4e232ce8c29b69a2c8
Boutaris <tboutaris@webershandwick.com>
2019-05-16 10:15:19 +0100

    added more tests for add_editable_column

--- tests/testthat/test_add_editable_css.R
+++ tests/testthat/test_add_editable_css.R
@@ -42,3 +42,28 @@ test_that("Function outputs the correct HTML", {
                    tHTML %>% add_editable_column(columns = -1:3)))
 
 })
+
+
+test_that("Function outputs the correct HTML with column names", {
+
+  tHTML <- tableHTML(mtcars,
+                     rownames = TRUE,
+                     widths = c(150, 100, rep(50, 11)),
+                     row_groups = list(c(10, 10, 12), c('Group 1', 'Group 2', 'Group 3'))) %>%
+    add_css_column(css = list('background-color', 'lightgray'), columns = 'row_groups') %>%
+    add_css_column(css = list('text-align', 'right'), columns = 'row_groups') %>%
+    add_css_header(css = list('background-color', 'lightgray'), headers = 1)
+
+  #rows ok
+  expect_true(grepl('id="tableHTML_rownames"><div contenteditable>',
+                    tHTML %>% add_editable_column(columns = c('row_groups', 'rownames', 'hp'))))
+
+  #row_groups ok
+  expect_true(grepl('id="tableHTML_row_groups" style="text-align:right;background-color:lightgray;" rowspan="12"><div contenteditable>',
+                    tHTML %>% add_editable_column(columns = c('row_groups', 'rownames', 'hp'))))
+
+  #column 1 ok
+  expect_true(grepl('<td id="tableHTML_column_1"><div contenteditable>',
+                    tHTML %>% add_editable_column(columns = c('row_groups', 'rownames', 'mpg'))))
+
+})
