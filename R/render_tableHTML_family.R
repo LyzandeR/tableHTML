06832b476b305868c8ecfc647403cfb1bae332d6
Teo <teoboot2007@hotmail.com>
2025-09-09 17:02:35 +0100

    fixed roxygen for uioutput

--- R/render_tableHTML_family.R
+++ R/render_tableHTML_family.R
@@ -1,17 +1,17 @@
 #' Implementing tableHTML in shiny
-#' 
+#'
 #' This function is used to implement tableHTML in a shiny app. It is used in the shiny
 #' ui.R file. Internally, it just calls uiOutput, since tableHTML creates HTML code.
 #'
 #' @param outputId input name.
 #' @param ... Other arguments to passed along to [shiny::uiOutput()]
 #'
-#' @seealso [uiOutput()]
+#' @seealso [shiny::uiOutput()]
 #' @md
 #'
 #' @examples
 #' \dontrun{
-#' 
+#'
 #' library(shiny)
 #' shinyApp(
 #'  ui = fluidPage(
@@ -20,22 +20,22 @@
 #'   br(),
 #'   column(width = 1),
 #'   tableHTML_output("mytable"))
-#'  ), 
+#'  ),
 #' server = function(input, output) {
-#'  output$mytable <- render_tableHTML( 
+#'  output$mytable <- render_tableHTML(
 #'   tableHTML(mtcars)
 #'  )}
 #' )
-#' 
+#'
 #' }
-#' 
+#'
 #' @export
 tableHTML_output <- function(outputId, ...) {
   shiny::uiOutput(outputId, ...)
 }
 
 #' Implementing tableHTML in shiny
-#' 
+#'
 #' This function is used to implement tableHTML in a shiny app. This function is used in the shiny
 #' server.R file. Internally, it just calls renderUI, since tableHTML creates HTML code.
 #'
@@ -47,7 +47,7 @@ tableHTML_output <- function(outputId, ...) {
 #'
 #' @examples
 #' \dontrun{
-#' 
+#'
 #' library(shiny)
 #' shinyApp(
 #'  ui = fluidPage(
@@ -56,15 +56,15 @@ tableHTML_output <- function(outputId, ...) {
 #'   br(),
 #'   column(width = 1),
 #'   tableHTML_output("mytable"))
-#'  ), 
+#'  ),
 #' server = function(input, output) {
-#'  output$mytable <- render_tableHTML( 
+#'  output$mytable <- render_tableHTML(
 #'   tableHTML(mtcars)
 #'  )}
 #' )
-#' 
+#'
 #' }
-#' 
+#'
 #' @export
 render_tableHTML <- function(expr, ...) {
  shiny::renderUI(expr, ...)
